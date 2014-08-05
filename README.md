d67940fdbbca958784667e6e37fdcd40dd8255cd
Trey Davis <trey@pogoseat.com>
2014-08-05 09:52:12 -0700

    Start somewhere

---
+++ README.md
@@ -0,0 +1,8 @@
+Magit Repo Menu
+===============
+
+A simple way to `magit-status` local and remote git repositories by selecting an item from a list.
+
+###Keys
+`g` refresh
+`<RET>` run `magit-status` with this repo
---
+++ magit-repo-menu.el
@@ -0,0 +1,31 @@
+(define-derived-mode repo-menu-mode tabulated-list-mode "Repo Menu" 
+  "Major mode repo-menu-mode, for displaying a list of repos, sourced from a special file"
+  (setq tabulated-list-format [("repo" 40 nil)
+                               ("location" 12 nil)])
+  (setq tabulated-list-padding 2)
+  (tabulated-list-init-header)
+  (define-key repo-menu-mode-map (kbd "RET") 'magit-connect)
+  (define-key repo-menu-mode-map (kbd "g") 'print-list))
+
+(defun magit-connect ()
+  (interactive)
+  (message (concat "current line ID is: " (tabulated-list-get-id)))
+  (magit-status (tabulated-list-get-id)))
+
+(defun get-string-from-file (filePath)
+  "Return filePath's file content."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+(defun print-list ()
+  (interactive)
+  (setq tabulated-list-entries (read (get-string-from-file "reposlist")))
+  ;(message "%s" 'tabulated-list-entries)
+  (tabulated-list-print t))
+
+(defun magit-repo-menu ()
+  (interactive)
+  (pop-to-buffer "*REPO MENU MODE*" nil)
+  (repo-menu-mode)
+  (print-list))
